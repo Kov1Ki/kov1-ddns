19940ce19afb18a037c09147a12b9df213c52e19
Kov1Ki <784725474@qq.com>
2025-05-31 00:28:13 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # Cloudflare DDNS 安装脚本
 # 使用方法: bash <(curl -sL https://raw.githubusercontent.com/Kov1Ki/kov1-ddns/main/install.sh)
 
-REPO_URL="https://raw.githubusercontent.com/Kov1Ki/kov1-ddns/main/cf-ddns.sh"
+REPO_URL="https://raw.githubusercontent.com/Kov1Ki/kov1-ddns/main"
 
 echo -e "\033[1;33m正在下载 Cloudflare DDNS 脚本...\033[0m"
 curl -sL -o /tmp/cf-ddns.sh "${REPO_URL}/cf-ddns.sh"
